dce61c3f728ddef3c10ef7ab60f4e777bc025d07
Duncan Parkes <duncan@mysociety.org>
2015-05-21 10:53:04 +0100

    Get names, parties and email addresses
    Fun with PyExecJs needed to get the email addresses.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,50 @@
+import re
+from urlparse import urljoin
+
+import requests
+import lxml.html
+import execjs
+
+from HTMLParser import HTMLParser
+unescape = HTMLParser().unescape
+
+source_url = 'http://www.conseil-national.mc/index.php/les-elus/les-groupes-politiques'
+
+resp = requests.get(source_url)
+root = lxml.html.fromstring(resp.text)
+
+alire = root.get_element_by_id('alire')
+
+data = []
+
+for heading in alire.cssselect('h4'):
+    party = heading.text_content().strip()
+
+    for li in heading.getnext().cssselect('li'):
+        member = {'party': party}
+        member_a = li.find('a')
+
+        member['name'] = member_a.text.strip()
+        details_url = member['details_url'] = urljoin(source_url, member_a.get('href'))
+
+        member_resp = requests.get(details_url)
+        member_root = lxml.html.fromstring(member_resp.text)
+
+        mailto_script = member_root.xpath("//h4[contains(., 'Contact Mail')]")[0].getnext().getchildren()[0].text_content()
+
+        # Get hold of the lines of javascript which aren't fiddling with the DOM
+        jslines = [x.strip() for x in re.search(r'<!--(.*)//-->', mailto_script, re.M | re.S).group(1).strip().splitlines() if not x.strip().startswith('document')]
+
+        # The name of the variable containing the variable containing the email address
+        # varies, so find it by regex.
+        varname = re.search(r'var (addy\d+)', mailto_script).group(1)
+        jslines.append('return {}'.format(varname))
+
+        js = '(function() {{{}}})()'.format(' '.join(jslines))
+        member['email'] = unescape(execjs.eval(js))
+
+        data.append(member)
+
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
